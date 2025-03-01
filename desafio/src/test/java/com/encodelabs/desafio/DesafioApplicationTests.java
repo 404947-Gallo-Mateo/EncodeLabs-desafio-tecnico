ac8ffe18b8a8ac54b93ca747bc28430204300045
404947_Gallo_Mateo <404947@tecnicatura.frc.utn.edu.ar>
2025-03-01 14:57:23 -0300

    100% de coverage

--- desafio/src/test/java/com/encodelabs/desafio/DesafioApplicationTests.java
+++ desafio/src/test/java/com/encodelabs/desafio/DesafioApplicationTests.java
@@ -3,11 +3,14 @@ package com.encodelabs.desafio;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class DesafioApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> DesafioApplication.main(new String[] {}));
 	}
 
 }
